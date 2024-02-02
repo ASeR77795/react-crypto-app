7d51fb65c67b0d99478d62d962cee94a15abf905
Pavlo <winterspray@gmail.com>
2024-02-02 17:59:26 -0500

    init

---
+++ src/components/layout/AppSider.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const AppSider = () => {
+	const siderStyle = {
+		textAlign: 'center',
+		lineHeight: '120px',
+		color: '#fff',
+		backgroundColor: '#1677ff',
+	};
+
+	return (
+		<Layout.Sider width='25%' style={siderStyle}>
+			Sider
+		</Layout.Sider>
+	);
+};
+
+export default AppSider;
